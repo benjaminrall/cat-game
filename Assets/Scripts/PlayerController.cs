9ca94f96c21e82f7e59b19b73d008d8b6ff6a1fe
benjaminrall <benr2508@aol.com>
2021-12-28 14:53:15 +0000

    started movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
